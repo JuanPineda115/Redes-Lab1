0118d48425777b12f7cfa6bc1f6c520cbdf8f0b8
JuanPineda115 <pin19087@uvg.edu.gt>
2022-08-01 15:54:07 -0600

    comments

--- emisor.py
+++ emisor.py
@@ -9,29 +9,30 @@ HOST = "127.0.0.1"
 PORT = 65432      
 
 def addNoise(message):
+    #primero hacemos encoding a ASCII
     bytesMessage = bytearray(message, 'ASCII')
-    print('antes del ruido', message)
-    #agregar el ruido
     for j in bytesMessage:
-        print(j)
+        #probabilidades del 10% de que la letra sea alterada
         odds = random.randint(1,10)
         if(odds == 1):
-            #si j es mayor a 200 le restaremos al valor ascii hasta un maximo de 255 para dejarlo en 0 en caso de salir ese codigo
+            #Si el codigo ascii es muy alto se le restara un offset para que no se pase del limite
             if(j >= 93):
+                #offset random
                 offset = random.randint(1, 28)
-                print('si hace algo el offset?', offset)
+                #aplicamos el offset
                 bytesMessage[bytesMessage.index(j)] -= offset
-            #si j es menor a 200, le sumaremos al valor ascii hasta un maximo de 56 para dejarlo en 255 en caso de salir 199
+            #si el codigo ascii de la letra es muy bajo, se le sumara para que no se quede un numero negativo o 0
             elif(j < 93):
+                #offset random
                 offset = random.randint(1, 28)
-                print('si hace algo el offset?', offset)
+                #aplicamos el offset
                 bytesMessage[bytesMessage.index(j)] += offset
-    print('despues del ruido', bytesMessage)
-    #ya que el mensaje quedo como un array, lo juntamos en bytes
     return bytesMessage
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #conexion del socket
     s.connect((HOST, PORT))
+    #ciclo while para enviar todos los mensajes sin ejecutar varias veces
     while True:
         userMessage = input("Ingrese su mensaje: ")
         finalMessage = addNoise(userMessage)
