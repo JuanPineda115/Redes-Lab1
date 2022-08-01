d9666cb2da23406eac0efb7edab3ff1db8088aec
JuanPineda115 <pin19087@uvg.edu.gt>
2022-07-31 21:00:12 -0600

    ascii version

--- emisor.py
+++ emisor.py
@@ -2,41 +2,37 @@
 #Redes
 #Diego Crespo 19541
 #Juan Pablo Pineda 19087
-from audioop import add
-from email import message
-from operator import index
 import socket
-from random import randint
+import random
 
 HOST = "127.0.0.1"  
 PORT = 65432      
 
 def addNoise(message):
-    binary = ' '.join(format(ord(x), 'b') for x in message)
-    bitlist = []
-    for j in binary:
-        bitlist.append(j)
+    bytesMessage = bytearray(message, 'ASCII')
+    print('antes del ruido', message)
     #agregar el ruido
-    noiseArray = []
-    for j in bitlist:
-        odds = randint(1,10)
+    for j in bytesMessage:
+        print(j)
+        odds = random.randint(1,10)
         if(odds == 1):
-            if(j == '1'):
-                j  = 0
-                noiseArray.append(j)
-            elif(j == '0'):
-                j = 1
-                noiseArray.append(j)
-        else:
-            noiseArray.append(j)
+            #si j es mayor a 200 le restaremos al valor ascii hasta un maximo de 255 para dejarlo en 0 en caso de salir ese codigo
+            if(j >= 93):
+                offset = random.randint(1, 28)
+                print('si hace algo el offset?', offset)
+                bytesMessage[bytesMessage.index(j)] -= offset
+            #si j es menor a 200, le sumaremos al valor ascii hasta un maximo de 56 para dejarlo en 255 en caso de salir 199
+            elif(j < 93):
+                offset = random.randint(1, 28)
+                print('si hace algo el offset?', offset)
+                bytesMessage[bytesMessage.index(j)] += offset
+    print('despues del ruido', bytesMessage)
     #ya que el mensaje quedo como un array, lo juntamos en bytes
-    noisedMessage = ''.join(str(x) for x in noiseArray)
-    return noisedMessage
-
+    return bytesMessage
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
         userMessage = input("Ingrese su mensaje: ")
         finalMessage = addNoise(userMessage)
-        s.sendall(bytes(finalMessage, 'ASCII'))
+        s.sendall(finalMessage)
--- receptor.py
+++ receptor.py
@@ -8,6 +8,14 @@ import socket
 HOST = "127.0.0.1" 
 PORT = 65432       
 
+def BinarytoString(binary):
+    binary_int = int(binary,2)
+    byte_number = binary_int.bit_length() + 7 // 8
+    binary_array = binary_int.to_bytes(byte_number, "big")
+    ascii_text = binary_array.decode()
+    return ascii_text;
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -18,6 +26,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            
             print(f"Recibido: {data}")
             conn.sendall(data)
