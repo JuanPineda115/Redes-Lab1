ee4b47bce0f4a1d52fb541839815a8ca53566df2
JuanPineda115 <pin19087@uvg.edu.gt>
2022-08-01 17:39:39 -0600

    Fase 2 terminada

--- emisor.py
+++ emisor.py
@@ -2,36 +2,31 @@
 #Redes
 #Diego Crespo 19541
 #Juan Pablo Pineda 19087
-import socket
-from random import randint
-import pickle
+import socket, pickle, bitarray
+from random import randint, seed
 
 HOST = "127.0.0.1"  
-PORT = 65432      
+PORT = 65431      
 
+#capa de ruido (client):
+#Añade ruido al mensaje del usuario
+# y Serializa el mensaje con Pickle para ser enviado
 def addNoise(message):
-    binary = ' '.join(format(ord(x), 'b') for x in message)
-    bitlist = []
-    for j in binary:
-        bitlist.append(j)
+    bitlist = bitarray.bitarray()
+    bitlist.frombytes(message.encode('utf-8'))
     #agregar el ruido
-    noiseArray = []
     for j in bitlist:
-        odds = randint(1,100)
-        if(odds == 1):
-            if(j == '1'):
-                j  = 0
-                noiseArray.append(j)
-            elif(j == '0'):
-                j = 1
-                noiseArray.append(j)
-            else:
-                noiseArray.append(j)
-        else:
-            noiseArray.append(j)
-    #ya que el mensaje quedo como un array, lo juntamos en bytes
-    noisedMessage = ''.join(str(x) for x in noiseArray)
-    return noisedMessage
+        seed(bytearray(message, 'utf-8'))
+        #115 es el numero favorito de Pineda y siempre busca referenciarlo
+        odds = randint(15,115)
+        #probabilidad de 1% de realizar un cambio de bit
+        if(odds == 115):
+            if(j == 1):
+                bitlist[bitlist.index(j)]  = 0
+            elif(j == 0):
+                bitlist[bitlist.index(j)] = 1
+    bitlist = pickle.dumps(bitlist)
+    return bitlist
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -39,4 +34,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         userMessage = input("Ingrese su mensaje: ")
         finalMessage = addNoise(userMessage)
-        s.sendall(pickle.dumps(finalMessage))
+        #serializamos el mensaje con pickle al enviarlo
+        s.sendall(finalMessage)
--- receptor.py
+++ receptor.py
@@ -2,11 +2,21 @@
 #Redes
 #Diego Crespo 19541
 #Juan Pablo Pineda 19087
-import socket, pickle
+import socket, pickle, bitarray
 
 
 HOST = "127.0.0.1" 
-PORT = 65431       
+PORT = 65431
+
+#capa de ruido (host):
+#Recibe el dato crudo y lo carga con pickle
+# Limpia el mensaje en caso de ser interrumpido por ruido.
+
+def supressNoise(message):
+    #Carga el mensaje con pickle y lo pasa a bytes
+    received = pickle.loads(message).tobytes()
+    #TODO Limpieza del mensaje
+    return received
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -18,6 +28,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            
-            print(f"{addr[1]}: {data}")
+            #extraemos el mensaje con pickle
+            received = supressNoise(data)
+            #lo pasamos a bytes para mostrarlo adecuadamente
+            print(f"{addr[1]} dice: {received}")
             conn.sendall(data)
